672863b3f9ab2ddaa942108314a86760c42a3e5d
edwincmj <80372170+edwincmj@users.noreply.github.com>
2022-04-19 12:33:26 -0400

    Log in/out config

--- src/main/java/com/example/controller/SimpleController.java
+++ src/main/java/com/example/controller/SimpleController.java
@@ -1,6 +1,5 @@
-package com.webcontent.controller;
+package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
