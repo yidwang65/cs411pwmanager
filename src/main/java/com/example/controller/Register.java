b0b7cdfa7119fc84e4fa98bf03ab73615197a6b8
edwincmj <80372170+edwincmj@users.noreply.github.com>
2022-04-22 14:15:48 -0400

    rename

--- src/main/java/com/example/controller/Register.java
+++ src/main/java/com/example/controller/Register.java
@@ -13,7 +13,7 @@ import org.springframework.validation.BindingResult;
 
 
 import java.sql.*;
-import com.example.Model.Newuser;
+import com.example.model.Newuser;
 
 @RequestMapping(value = "/Register", method = RequestMethod.POST)
 public class Register {
