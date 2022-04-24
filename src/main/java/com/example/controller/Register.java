a46032cdb4b1cb2e68f219c824e18e8404e4f519
lchen456 <lesleychen456@gmail.com>
2022-04-24 17:55:18 -0400

    register
    this should actually be named registeruser

--- src/main/java/com/example/controller/Register.java
+++ src/main/java/com/example/controller/Register.java
@@ -4,9 +4,11 @@ package com.example.controller;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 
 
@@ -15,42 +17,41 @@ import org.springframework.validation.BindingResult;
 import java.sql.*;
 import com.example.Model.Newuser;
 
-@RequestMapping(value = "/Register", method = RequestMethod.POST)
+@Controller
 public class Register {
     static final String myUrl = "jdbc:mysql://localhost/pwmanager";
     static final String USER =
             "root";
-    static final String PASS = "root";
+    static final String PASS = "criesinpain";
+    
+    @PostMapping(value = "/Register")
+    public static String register(@RequestParam(name = "first_name") String firstname, 
+    		@RequestParam(name = "last_name") String lastname, @RequestParam(name = "email") String email, 
+    		@RequestParam(name = "password1") String password1, @RequestParam(name = "password2") String password2  ) { 
 
-    public static void register(@Valid @ModelAttribute("user")Newuser user, 
-      BindingResult result) { 
-         if(result.hasErrors()){
-            return;
-         }
-
-         if(!(user.getPassword1()).equals(user.getPassword2())){
-            return;
-         }
+    	if(!(password1.equals(password2))) {
+    		return null;
+    	}
 
         // Connecting to DB
         try{
             Connection conn = DriverManager.getConnection(myUrl, USER, PASS);
             // the mysql insert statement
-            String query = " insert into users (uid, first_name, last_name, email, password)"
-            + " values (?, ?, ?, ?, ?)";
+            String query = " insert into users (first_name, last_name, email, password)"
+            + " values (?, ?, ?, ?)";
 
             PreparedStatement preparedStmt = conn.prepareStatement(query);
-            preparedStmt.setInt(1, user.getUid());
-            preparedStmt.setString(2, user.getFirst_name());
-            preparedStmt.setString(3, user.getLast_name());
-            preparedStmt.setString(4, user.getEmail());
-            preparedStmt.setString(4, user.getPassword1());
+            preparedStmt.setString(1, firstname);
+            preparedStmt.setString(2, lastname);
+            preparedStmt.setString(3, email);
+            preparedStmt.setString(4, password1);
             conn.close();
+            return null;
         }catch(SQLException e) {
             e.printStackTrace();
         }
 
-
+        return "hello";
     }
 
 
